eb2b40b5d1261e827325b1ecbf311f92a644dc72
Diletta Calussi <s0559842@htw-berlin.de>
2018-11-19 13:05:03 +0100

    Zwischenstauts - Uebung 19.11

--- songsServlet/src/main/java/de/htw/ai/kbe/songsServlet/Songs.java
+++ songsServlet/src/main/java/de/htw/ai/kbe/songsServlet/Songs.java
@@ -1,4 +1,12 @@
 package de.htw.ai.kbe.songsServlet;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 
  * @author dixen
@@ -6,4 +14,58 @@ package de.htw.ai.kbe.songsServlet;
  *
  */
 public class Songs {
+	/**
+	 * Stores the new songs in the in memory database
+	 */
+	private static Map<Integer, Song> storage;
+	
+	
+	/**
+	 * The DB instance
+	 */
+	private static Songs instance = null;
+	
+	private Songs() {
+		storage = new ConcurrentHashMap<Integer,Song>();
+	}
+	
+	//Singleton
+	public synchronized static Songs getInstance(List<Song> songsFromJsonFile) {
+		if(instance == null) {
+			instance = new Songs();
+			initSongsDB(songsFromJsonFile);
+		} 
+		return instance;
+	}
+	
+	private static void initSongsDB(List<Song> songsFromJsonFile) {
+		//Reads songs from songs.json and stores them in the hashmap
+		for (Song s: songsFromJsonFile) {
+			instance.addSong(s);
+		}
+	}
+	
+	/**
+	 * returns the song with songId
+	 * @param songId
+	 * @return
+	 */
+	public Song getSong(Integer songId) {
+		return storage.get(songId);
+	}
+	
+	public Collection<Song> getAllSongs(){
+		return storage.values();
+	}
+	
+	public Integer addSong(Song song) {
+		//Add song in the Hashmap
+		song.setId((int) storage.keySet().stream().count()+1);
+		storage.putIfAbsent(song.getId(), song);
+		return song.getId();
+	}
+	
+	public Integer getLastID() {
+		return (int) (storage.keySet().stream().count()+1);
+	}
 }
--- songsServlet/src/main/java/de/htw/ai/kbe/songsServlet/SongsServlet.java
+++ songsServlet/src/main/java/de/htw/ai/kbe/songsServlet/SongsServlet.java
@@ -1,4 +1,5 @@
 package de.htw.ai.kbe.songsServlet;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -6,40 +7,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//import org.json.JSONObject;
+
 /**
  * Manages HTTP requests and responses (and JSON parsing)
+ * 
  * @author dixen
  *
  */
 public class SongsServlet extends HttpServlet {
 
-
-	//Einige Methoden aus HttpServlet ueberschrieben
-	
 	@Override
-    public void init(ServletConfig config) throws ServletException {
-       
-    }
+	public void init(ServletConfig config) throws ServletException {
 
-    @Override
-    public void init() throws ServletException {
-       
-    }
-	
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       
-    }
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-    }
+	@Override
+	public void init() throws ServletException {
 
-    @Override
-    public void destroy() {
-        
-    }
+	}
 
-    
+	//http://localhost:8080/songsServlet?all mit Accept-Header: * oder application/json oder ohne Accept-Header soll alle gespeicherten Songs in JSON-Format zurücksenden
+	//http://localhost:8080/songsServlet?songId=6 mit Accept-Header: * oder application/json oder ohne Accept-Header soll den Song 6 in JSON-Format zurücksenden
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+	//http://localhost:8080/songsServlet mit Payload soll eine neue ID fuer den neuen Song generieren und den Song in der DB speichern
+	//neue ID an den Client als Wert des "Location"-Header: http://localhost:8080/songsServlet?songId=newId der Response zurückschicken (Body ist leer)
+	//akzeptiert nur JSON payloads
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+	@Override
+	public void destroy() {
+		//TODO: Content from Hashmap should be transferred to songs.json
+
+	}
+
 }
