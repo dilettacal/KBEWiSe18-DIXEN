e52c4839e0e34eb94cfed7b6a5f15893dfa607eb
dltcls <s0559842@htw-berlin.de>
2018-12-15 10:43:36 +0100

    AuthWebService und Tests dafür funktionieren alle gut

--- songsRX/src/main/java/de/htw/ai/kbe/services/AuthWebService.java
+++ songsRX/src/main/java/de/htw/ai/kbe/services/AuthWebService.java
@@ -1,21 +1,16 @@
 package de.htw.ai.kbe.services;
 
-import java.util.UUID;
 
 import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import de.htw.ai.kbe.bean.User;
-import de.htw.ai.kbe.filter.AuthenticationFilter;
 import de.htw.ai.kbe.filter.IAuth;
-import de.htw.ai.kbe.storage.IUser;
-import de.htw.ai.kbe.storage.UserStorage;
 
 //URL fuer diesen Service ist: http://localhost:8080/songsRX/rest/auth?userId=schueler --> ghahoeho4
 @Path("/auth")
@@ -31,15 +26,14 @@ public class AuthWebService {
 	}
 	
 	@GET
-	@Path("/")
+	//@Path("/")
 	@Produces({ MediaType.TEXT_PLAIN })
 	public Response getToken(@QueryParam("userId") String userID) {
 		System.out.println("USERID: " + userID); //liefert eschuler
-		System.out.println(userID.trim()); //liefert eschuler
-		if(userID == null || (userID.isEmpty())) {
-			return Response.status(Response.Status.BAD_REQUEST).entity(
-					Response.Status.BAD_REQUEST + ": No username given!")
-					.build();
+		System.out.println(userID==null);
+		if(userID == null || userID.trim().isEmpty() || userID.equals("")) {
+			//Laesst sich besser testen
+			throw new BadRequestException("No userId"); //400 and token is null
 		}
 		
 		String token = tokenStorage.authenticate(userID);
--- songsRX/src/test/java/de/htw/ai/kbe/services/AuthWebServiceTest.java
+++ songsRX/src/test/java/de/htw/ai/kbe/services/AuthWebServiceTest.java
@@ -27,44 +27,56 @@ public class AuthWebServiceTest extends JerseyTest {
 			@Override
 			protected void configure() {
 				bind(SongsStorage.class).to(ISongs.class).in(Singleton.class);
-				bind(UserStorage.class).to(IUser.class); //.in(Singleton.class);
+				bind(UserStorage.class).to(IUser.class); // .in(Singleton.class);
 				bind(AuthTokenStorage.class).to(IAuth.class).in(Singleton.class);
 
 			}
 		});
 	}
 
+	// ==== AUTH GET WITH VALID USERID ==== //
+	//rest/auth?userId=mmuster --> 200 and token
 	@Test
-	public void validUserIdToAuthShouldReturn200() {
+	public void getWithvalidUserIdToAuthShouldReturn200AndAToken() {
 		Response response = target("/auth").queryParam("userId", "mmuster").request().get();
-		System.out.println("Valid UserId Request: " + response.getStatus());
+		System.out.println(response.getStatus());
 		Assert.assertEquals(200, response.getStatus());
+		String token = response.readEntity(String.class);
+		assertNotNull(token);
+		System.out.println(token);
 	}
 
+	
+	// === AUTH GET WITH NOT VALID USERID ===
+	// not existing userId --> Unauthorized
 	@Test
-	public void NoValidUserIdToAuthShouldReturn401() {
+	public void getWithNoValidUserIdToAuthShouldReturn401UnAuthorized() {
 		Response response = target("/auth").queryParam("userId", "muster").request().get();
-		System.out.println("Non Valid UserId Request: " + response.getStatus());
-		Assert.assertEquals(401, response.getStatus());
+		String resp = response.readEntity(String.class);
+		assertEquals(401, response.getStatus());		
+        assertTrue(resp.contains("Unauthorized"));		
 	}
 
+	//emtpy userId --> Bad Request
 	@Test
-	public void NoUserIdToAuthShouldReturn403() {
-		//TODO: Fix me im Code - Falls QueryParam nicht uebergeben werden!
-		Response response = target("/auth").request().get();
-		System.out.println("Non Valid UserId Request: " + response.getStatus());
-		Assert.assertEquals(403, response.getStatus());
-	}
-
-	@Test
-	public void testGetWithEmptyUserIdShouldReturnBadRequest() { 
-	//Dieser Teil ist erfolgreich
+	public void getWithEmptyUserIdToAuthShouldReturn400BadRequest() {
 		Response response = target("/auth").queryParam("userId", "").request().get();
-
-		assertEquals(400, response.getStatus());
-		//FIXME: Dieser Teil liefert AssertionError
-//		String token = response.readEntity(String.class);
-//		assertTrue(token == null || token.isEmpty());
+		System.out.println(response.getStatus());
+		Assert.assertEquals(400, response.getStatus());
+		String token = response.readEntity(String.class);
+        assertTrue(token == null || token.isEmpty());		
 	}
+
+	//request without params --
+	@Test
+	public void NoUserIdToAuthShouldReturn400BadRequest() {
+		// Request ohne Parameter: rest/auth
+		Response response = target("/auth").request().get();
+		//userId is null
+		System.out.println(response.getStatus());
+		Assert.assertEquals(400, response.getStatus());
+	}
+
+	
 
 }
